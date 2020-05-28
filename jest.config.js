9bc7c3cac53101c2bfea198914462d6a1e4293f8
Thiago Marques Cavalcante <thiago.marques@interno.evolutecnet.com.br>
2020-05-28 11:06:04 -0300

    chore: jest config mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
-  '!<rootDir>/src/**/*-protocols.ts/',
-  '!**/protocols/**',
-  '!**/test/**'
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts/',
+    '!**/protocols/**',
+    '!**/test/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
